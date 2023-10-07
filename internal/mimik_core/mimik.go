5fa7e0f68ea9f6adf1b8a3e59f17b7a7493895cb
bharathappali <bharath.appali@gmail.com>
2023-10-08 00:10:57 +0530

    Use Consts
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- internal/mimik_core/mimik.go
+++ internal/mimik_core/mimik.go
@@ -3,40 +3,15 @@ package mimik_core
 import (
 	"fmt"
 	"mimik/internal/data"
+	"mimik/internal/utils"
 )
 
 func CreateNamespaceFiles(flags data.LaunchFlags) {
 	fmt.Println("Creating Namespace files")
 
-	var limitReached bool = false
-	var locationsCovered uint16 = 0
+	var selectedData data.SelectedContent = utils.ExtractRequiredLocations(flags)
 
-	var selectedData = data.SelectedContent{
-		ContinentCountryLocationMap: make(map[string]map[string][]string),
-		Validated:                   false, // Initially not validated
+	if selectedData.Validated == true {
+
 	}
-
-	for continent, countries_map := range data.ContinentCountryLocationMap {
-		for country, locations_list := range countries_map {
-			locationsCovered = locationsCovered + 1
-
-			if _, ok := selectedData.ContinentCountryLocationMap[continent]; !ok {
-				selectedData.ContinentCountryLocationMap[continent] = make(map[string][]string)
-			}
-
-			var continent_map = selectedData.ContinentCountryLocationMap[continent]
-			if _, ok := continent_map[country]; !ok {
-				continent_map[country] = locations_list
-			}
-			if locationsCovered >= flags.Clusters {
-				limitReached = true
-				break
-			}
-		}
-		if limitReached {
-			break
-		}
-	}
-
-	fmt.Println(selectedData)
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -18,7 +18,7 @@ func ValidateFlags(cmd *cobra.Command) data.LaunchFlags {
 	validator.Validated = true
 
 	// Validate clusters flag
-	if clusters >= 2 && clusters < 199 {
+	if clusters >= MinClusters && clusters <= MaxClusters {
 		validator.Clusters = uint16(clusters)
 	} else {
 		fmt.Printf("%sInvalid clusters: %d | Valid values: Min Clusters - %d, Max Clusters - %d\n", LogErrorPrefix, clusters, MinClusters, MaxClusters)
@@ -29,7 +29,7 @@ func ValidateFlags(cmd *cobra.Command) data.LaunchFlags {
 	validator.RunMimik = runMimik
 
 	// Validate days flag
-	if days > 14 && days < 366 {
+	if days >= MinDays && days <= MaxDays {
 		validator.Days = days
 	} else {
 		fmt.Printf("%sInvalid days: %d | Valid values: Min Days - %d, Max Days - %d\n", LogErrorPrefix, days, MinDays, MaxDays)
@@ -37,4 +37,39 @@ func ValidateFlags(cmd *cobra.Command) data.LaunchFlags {
 	}
 
 	return validator
+}
+
+func ExtractRequiredLocations(flags data.LaunchFlags) data.SelectedContent {
+	var limitReached bool = false
+	var locationsCovered uint16 = 0
+
+	var selectedData = data.SelectedContent{
+		ContinentCountryLocationMap: make(map[string]map[string][]string),
+		Validated:                   false, // Initially not validated
+	}
+
+	for continent, countries_map := range data.ContinentCountryLocationMap {
+		for country, locations_list := range countries_map {
+			locationsCovered = locationsCovered + 1
+
+			if _, ok := selectedData.ContinentCountryLocationMap[continent]; !ok {
+				selectedData.ContinentCountryLocationMap[continent] = make(map[string][]string)
+			}
+
+			var continent_map = selectedData.ContinentCountryLocationMap[continent]
+			if _, ok := continent_map[country]; !ok {
+				continent_map[country] = locations_list
+			}
+			if locationsCovered >= flags.Clusters {
+				limitReached = true
+				break
+			}
+		}
+		if limitReached {
+			selectedData.Validated = true
+			break
+		}
+	}
+
+	return selectedData
 }
